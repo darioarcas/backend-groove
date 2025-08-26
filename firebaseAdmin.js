f40878c8e445a0109db361b040392f885869924b
darioarcas <darioarcas@hotmail.com>
2025-08-26 00:52:03 -0300

    Config firebase admin para usar variable de entorno en producción

--- firebaseAdmin.js
+++ firebaseAdmin.js
@@ -1,11 +1,22 @@
 // backend/firebaseAdmin.js
 
 const admin = require('firebase-admin');
-const serviceAccount = require('./serviceAccountKey.json'); // archivo que vas a descargar
+
+let serviceAccount;
+
+if (process.env.FIREBASE_SERVICE_ACCOUNT) {
+  // Parsear el JSON guardado como variable de entorno
+  serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+} else {
+  // En desarrollo, cargar localmente el archivo
+  serviceAccount = require('./serviceAccountKey.json');
+}
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
 
 const db = admin.firestore();
+
 module.exports = { admin, db };
+
