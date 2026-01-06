2c1a6c666d865b582f61a9e3c26dd3e2af73c760
darioarcas <darioarcas@hotmail.com>
2026-01-06 09:11:31 -0300

    creando suscripcion mensual

--- services/mercadoPagoService.js
+++ services/mercadoPagoService.js
@@ -10,7 +10,7 @@ if (admin.apps.length === 0) {
 }
 
 
-const db = admin.firestore();  // Acceso a Firestore (<-------------ACA FALLA)
+const db = admin.firestore();  // Acceso a Firestore
 
 
 const crearPreferenciaPago = async ({ cursoNombre, cursoId, uid, base_url }) => {
@@ -76,4 +76,82 @@ const crearPreferenciaPago = async ({ cursoNombre, cursoId, uid, base_url }) => 
   }
 };
 
-module.exports = { crearPreferenciaPago };
+
+
+
+
+
+
+const crearSuscripcion = async ({ cursoNombre, cursoId, uid, base_url }) => {
+  const allowedOrigins = ['https://academiagroove.com', 'https://darioarcas.github.io', 'http://localhost:3000'];
+  if (!allowedOrigins.includes(base_url)) {
+    throw new Error('Origen no permitido');
+  }
+
+  if (base_url === 'https://darioarcas.github.io' || base_url === 'http://localhost:3000') {
+    base_url += '/pagina-groove-inicio-de-sesion/#';
+  }
+
+  try {
+    const cursoRef = db.doc(`cursos_privados/${cursoId}`);
+    const cursoDoc = await cursoRef.get();
+
+    if (!cursoDoc.exists) {
+      throw new Error('Curso no encontrado');
+    }
+
+    const cursoData = cursoDoc.data();
+    const precio = cursoData.precio;
+
+    const successUrl = `${base_url}/perfil`;
+    const failureUrl = `${base_url}/error-pago`;
+
+    // Creamos la preferencia de suscripción
+    const preference = {
+      items: [
+        {
+          title: cursoNombre || 'Curso',
+          quantity: 1,
+          unit_price: precio,
+          currency_id: 'ARS',
+        },
+      ],
+      back_urls: {
+        success: successUrl,
+        failure: failureUrl,
+      },
+      notification_url: `https://backend-groove-pi69.onrender.com/api/webhook/mercadopago`,
+      external_reference: `${cursoId}_${uid}`,
+      auto_return: 'approved',
+      subscription: {
+        frequency: 1, // Frecuencia del pago (1 para mensual)
+        frequency_type: 'months', // Tipo de frecuencia (meses)
+        transaction_amount: precio,
+        currency_id: 'ARS',
+        back_urls: {
+          success: successUrl,
+          failure: failureUrl,
+        },
+      }
+    };
+
+    const response = await fetch('https://api.mercadopago.com/checkout/preferences', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${process.env.MERCADOPAGO_ACCESS_TOKEN}`,
+      },
+      body: JSON.stringify(preference),
+    });
+
+    const data = await response.json();
+    console.log("✅ Preferencia de suscripción creada:", data);
+    return data.init_point;
+  } catch (error) {
+    console.error('❌ Error creando preferencia de suscripción:', error);
+    throw error;
+  }
+};
+
+
+module.exports = { crearPreferenciaPago, crearSuscripcion };
